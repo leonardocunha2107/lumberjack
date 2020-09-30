0247fb927da628d8fd98b47d43ef591a9fd03fd5
Leonardo Cunha <leonardocunha2107@gmail.com>
2020-09-30 16:12:47 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+
+# This call to setup() does all the work
+setup(
+    name="lumberjack",
+    version="1.0.0",
+    description="A small module to fit your ML experiments logging needs",
+    url="https://github.com/leonardocunha2107/lumberjack",
+    author="Real Python",
+    author_email="leonardocunha2107@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+
+    ],
+    packages=["lumberjack"],
+    include_package_data=True
+    )
+
+    
+
